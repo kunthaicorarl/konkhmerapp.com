5358b5796f044fd5e5bb99a8f725d8b5d0ffa66a
phonkunthai <kunthai.corarl@gmail.com>
2017-04-10 02:51:08 +0700

    create search

--- pages/noty/search.js
+++ pages/noty/search.js
@@ -1,4 +1,37 @@
-app.controller("pages.noty.search",['$scope',function($scope){
+app.controller("pages.noty.search",['$scope','$http','$state',function($scope,$http,$state){
          var vm=this;
            vm.title="TV";
+            vm.goTo=function(e){
+            $state.go("#/watch/"+e);
+            };
+
+            vm.searchMovie=function(){
+
+                vm.loading = true;
+              
+                  var req = {
+                      method: 'GET',
+                      url: 'http://konkhmerapp.com//wp-json/wp/v2/posts?search='+vm.filterText,
+                      headers: {
+                        'Content-Type': undefined
+                      }//,
+                      // data: { per_page:4,page:1,categories:6}
+                      };
+                   $http(req).success(function(data){
+                              vm.myItems=[];
+                            if(!data){
+                              console.log("Nothing...");
+                            }else{
+                              console.log(data);
+                             angular.forEach(data, function(value, key) {
+                              vm.myItems.push({link:value.link,title:value.title.rendered,img:angular.element(value.content.rendered).find('img')[0].src,id:value.id});
+                                 
+                            });
+                          }
+                       
+                            }).finally(function(){
+                            vm.loading = false;
+                            });
+             
+           };
 }]);
